c43c7840a3f34e99d68c22f6aebe6545491bf601
Rhuan-Messias <messias.numpy@gmail.com>
2022-10-14 17:44:16 -0300

    tela

--- snake.py
+++ snake.py
@@ -1,1 +1,28 @@
-print('snake_game')
+import turtle as t
+
+tela = t.Screen()
+tela.setup(width=600, height=600) #keyword arguments, quando o parâmetro recebe as informações, onde azul é o parametro e a o numero é o argumento chave
+tela.bgcolor('green')
+tela.title('Jogo da Cobrinha')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+tela.exitonclick()
